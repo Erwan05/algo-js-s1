f08dd26f57e45ef61e795805fd4c29ffd42c2fc6
Erwan05 <dionix654@gmail.com>
2023-09-26 16:37:07 +0100

    adding learning / exercice 1 et exercice 2

---
+++ Exercice 2/main.js
@@ -0,0 +1,13 @@
+var a = 4
+var b = 4
+var c = 3
+
+if (a = b) {
+    console.log("égal à b")
+}
+if (a = c) {
+    console.log("égale à c")
+}
+if (b = c) {
+    console.log("égale à rien")
+}
---
+++ learning/main.js
@@ -0,0 +1,12 @@
+//alert("Bonjour, veux tu vraiment quitter ce site ?")
+
+//console.log("bienvenue sur la console")
+
+var cardName = "choixpeau"
+
+if (6 > 7) {
+    console.log('oui') 
+}
+else {
+    console.log('non')
+}
